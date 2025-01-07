75b2a4447f5b3a8f6efaf84fc4b6c4e8e02c55a2
Libe <mrlibelula@gmail.com>
2025-01-07 03:57:26 -0400

    added new dev hosts

--- background.js
+++ background.js
@@ -10,6 +10,18 @@ const defaultCredentials = {
     '192.168.1.202:98': {
         email: import.meta.env.VITE_DEFAULT_EMAIL || 'luis@libe.dev',
         password: import.meta.env.VITE_DEFAULT_PASSWORD || ''
+    },
+    'https://libeflix.test/login': {
+        email: import.meta.env.VITE_LIBESOFT_EMAIL || 'libe@libesoft.io',
+        password: import.meta.env.VITE_LIBESOFT_PASSWORD || ''
+    },
+    'http://music.test:98/login': {
+        email: import.meta.env.VITE_DEFAULT_EMAIL || 'luis@libe.dev',
+        password: import.meta.env.VITE_DEFAULT_PASSWORD || ''
+    },
+    'http://libeflix.test:81/login': {
+        email: import.meta.env.VITE_LIBESOFT_EMAIL || 'libe@libesoft.io',
+        password: import.meta.env.VITE_LIBESOFT_PASSWORD || ''
     }
 };
 
--- config.js
+++ config.js
@@ -11,6 +11,18 @@ const config = {
         '192.168.1.202:81': {
             email: import.meta.env.VITE_LIBESOFT_EMAIL,
             password: import.meta.env.VITE_LIBESOFT_PASSWORD
+        },
+        'https://libeflix.test/login': {
+            email: import.meta.env.VITE_LIBESOFT_EMAIL,
+            password: import.meta.env.VITE_LIBESOFT_PASSWORD
+        },
+        'http://music.test:98/login': {
+            email: import.meta.env.VITE_DEFAULT_EMAIL,
+            password: import.meta.env.VITE_DEFAULT_PASSWORD
+        },
+        'http://libeflix.test:81/login': {
+            email: import.meta.env.VITE_LIBESOFT_EMAIL,
+            password: import.meta.env.VITE_LIBESOFT_PASSWORD
         }
     }
 };
